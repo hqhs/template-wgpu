4e7b4bcfafa5b37b7573f554b8f8f80e3abd69e5
Dima Afanasyev <dimaafanasev@example.com>
2023-11-19 20:06:08 +0400

    enable compiler warnings in cmake

--- bin/main.cpp
+++ bin/main.cpp
@@ -135,16 +135,17 @@ main(int, char **)
     WGPUSwapChain swapChain = wgpuDeviceCreateSwapChain(device, surface, &swapChainDesc);
 
     const char *vs = R"(
-        @vertex fn main(
-            @builtin(vertex_index) VertexIndex : u32
-        ) -> @builtin(position) vec4f {
-            var pos = array(
-                vec2f( 0.0,  0.5),
-                vec2f(-0.5, -0.5),
-                vec2f( 0.5, -0.5)
-            );
-            return vec4f(pos[VertexIndex], 0.0, 1.0);
-        })";
+@vertex
+fn main(@builtin(vertex_index) VertexIndex : u32) -> @builtin(position) vec4f
+{
+    var pos = array(
+        vec2f( 0.0,  0.5),
+        vec2f(-0.5, -0.5),
+        vec2f( 0.5, -0.5)
+    );
+    return vec4f(pos[VertexIndex], 0.0, 1.0);
+}
+)";
     WGPUShaderModule vsModule = createShaderModule(device, vs);
 
     wgpuShaderModuleRelease(vsModule);
